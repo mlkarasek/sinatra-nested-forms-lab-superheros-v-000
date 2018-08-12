f15d86e7af575f7b390c5e5eb3318bead65fc317
Monica  Karasek <mlkarasek@gmail.com>
2018-08-12 23:32:35 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -2,9 +2,12 @@ class Superhero
 
   attr_accessor :name, :power, :biography
 
+  @@superheroes = []
+
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @biography = params[:biography]
     @@superheroes << self
-  end 
+  end
+end 
